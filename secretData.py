6c856e9d87e0a8ceb477fb0d31a2980001417840
RocketonTON <rocketonton@uncensoredproject.xyz>
2026-02-09 03:17:35 +0100

    Update secretData.py

--- secretData.py
+++ secretData.py
@@ -4,4 +4,4 @@ import os
 # Leggi tutto dalle variabili d'ambiente
 bot_token = os.environ.get("TELEGRAM_BOT_TOKEN", "")
 cmc_token = os.environ.get("CMC_TOKEN", "")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
+notify_chat = os.environ.get("TELEGRAM_CHAT_ID", "")
