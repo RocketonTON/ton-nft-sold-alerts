9d2943e0d57c0425a8d6626d94ad68cccfef0c87
RocketonTON <rocketonton@uncensoredproject.xyz>
2026-02-09 02:57:00 +0100

    Update secretData.py

--- secretData.py
+++ secretData.py
@@ -1,3 +1,7 @@
-bot_token = ''
-cmc_token = ''
-notify_chat = ''
+# secretData.py (versione per variabili d'ambiente)
+import os
+
+# Leggi dalle variabili d'ambiente, con fallback per testing locale
+tonapi_token = os.environ.get("TONAPI_TOKEN", "")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
